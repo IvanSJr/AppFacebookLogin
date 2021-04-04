de22ca55952469cad2c19f3e9833da03dfca7370
IvanSJr <ivan.junior2706@gmail.com>
2021-04-03 23:43:43 -0300

    Alteração no nome da rota de login

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth import views as auth_views
 from main.views import IndexView, LoginView
 
 urlpatterns = [
-    path('logim/', LoginView.as_view(), name='login'),
+    path('login/', LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('social-auth/', include('social_django.urls', namespace='social')),
     path('', IndexView.as_view(), name='index'),
